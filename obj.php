061da8dfe564d5802849d6ef2d312287e0e4dc6b
grubbyLucifer <965482575@qq.com>
2017-03-19 20:38:11 +0800

    fist commit
    commit test file

---
+++ obj.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//echo phpinfo();exit;
+$obj1 = new ShopProductor();
+$obj2 = new ShopProductor();
+var_dump($obj1);
+var_dump($obj2);
+
+class ShopProductor {
+
+    function __construct() {
+        
+    }
+
+}
+
+
+?>
